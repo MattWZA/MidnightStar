22e13520bc8ac5cf6a7d48b58f9e70368e72f236
MattWZA <matthewwalker101@gmail.com>
2017-01-27 11:52:17 +0200

    Framework

---
+++ midnightstar.co.za/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Midnight Star | Desktop publishing and freelance DTP in Johannesburg, South Africa</title>
+
+    <!--Bootstrap CSS-->
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+  
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src='https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js'></script>
+      <script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'></script>
+    <![endif]-->
+
+    <!--Font-Awesome-->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    
+    <!--Custom CSS-->
+    <link rel='stylesheet' href='styles/styles.css'>
+  </head>
+  <body>
+
+    <!--Nav-->
+    <nav class='navbar navbar-fixed-top navbar-inverse'>
+      <div class='container'>
+        <!--brand and toggle-->
+        <header class='navbar-header'>
+          <a class='navbar-brand' href='/'>
+            Midnight Star
+          </a>
+          <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#menu'>
+            <span class='icon-bar'></span>
+            <span class='icon-bar'></span>
+            <span class='icon-bar'></span>
+          </button>
+        </header>
+        
+        <!--Menu-->
+        <div id='menu' class='collapse navbar-collapse'>
+          <ul class='nav navbar-nav'>
+            <li><a href='#'>Products</a></li>
+            <li><a href='#'>Deals</a></li>
+          </ul>
+          <ul class='nav navbar-nav navbar-right'>
+            <li><a href='#'><span class='glyphicon glyphicon-home'></span> Home</a></li>
+            <li><a href='#'><span class='glyphicon glyphicon-tag'></span> Services</a></li>
+            <li><a href='#'><span class='glyphicon glyphicon-envelope'></span> Contact Us</a></li>
+          <ul>
+        
+        </div>
+      </div>
+    </nav>
+
+    <!--Main Content-->
+    <main>
+      <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+                <h1>COntent</h1>
+            </div>
+          </div>
+      </div>
+    </main>
+
+    <!--Footer-->
+    <footer class='text-center'>
+      <nav class='navbar navbar-inverse navbar-fixed-bottom'>
+        <div class='container'>
+          <ul class='nav navbar-nav'>
+            <li class='navbar-text'>&copy Copyright <span class='thisYear'></span> Midnight Star</li>
+            <li><a href='https://www.facebook.com/Midnight-Star-128905187154590/' target='_blank'><i class='fa fa-facebook'></i> Facebook</a></li>
+          </ul>
+          <ul class='nav navbar-nav navbar-right'>
+            <li><a href='#'><span class='glyphicon glyphicon-envelope'></span> Contact Us</a></li>
+          </ul>
+        </div>      
+      </nav>
+    </footer>
+
+    <!--JAVASCRIPT-->
+
+    <!--JQuery-->
+    <script src='https://code.jquery.com/jquery-3.1.1.min.js' integrity='sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=' crossorigin='anonymous'></script>
+
+    <!--Bootstrap JS-->
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>
+    
+    <!--Custom JS-->
+    <script src='scripts/app.js'></script>
+  </body>
+</html>
